eaf5554d97741c8dc6aba42bdb385008c8cd5a7b
Junyou-Lin <joe15091510@gmail.com>
2022-09-30 15:14:27 +1000

    test serverless CI/CD

--- app/server.js
+++ app/server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.status(200).send('Spatial Vision Technical Task API')
+  res.status(200).send('Spatial Vision Technical Task API version 1.0')
 })
 
 app.use('/api/users', userRoute)
